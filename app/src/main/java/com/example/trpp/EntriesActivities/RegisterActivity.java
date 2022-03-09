f1e8d823599879d79b33ddfb0ed4b00ac2711cfe
Dmitry <lisyutin.dima@mail.com>
2022-03-09 22:02:57 +0300

    Добавлен SnackBar с кнопкой. Клавиатура теперь автоматически скрывается.

--- app/src/main/java/com/example/trpp/EntriesActivities/RegisterActivity.java
+++ app/src/main/java/com/example/trpp/EntriesActivities/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.trpp.EntriesActivities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,6 +11,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.trpp.R;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -28,7 +32,18 @@ public class RegisterActivity extends AppCompatActivity {
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Регистрация успешна", Toast.LENGTH_LONG).show();
+                //hide keyboard
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(register_button.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+                //end of hide
+                Snackbar RegisterSnackBar = Snackbar.make(view, "Регистрация успешна", BaseTransientBottomBar.LENGTH_LONG);
+                RegisterSnackBar.show();
+                RegisterSnackBar.setAction("Вход", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                });
             }
         });
     }
