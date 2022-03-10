322ef5847efcd9967bafea68c8276fb36cba354a
Dmitry <lisyutin.dima@mail.com>
2022-03-10 13:16:17 +0300

    Добавлен фрагмент очереди.

---
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/queue/QueueFragment.java
@@ -0,0 +1,38 @@
+package com.example.trpp.NavigationActivity.ui.queue;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.trpp.R;
+
+public class QueueFragment extends Fragment {
+
+    private QueueViewModel mViewModel;
+
+    public static QueueFragment newInstance() {
+        return new QueueFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_queue, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(QueueViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/queue/QueueViewModel.java
@@ -0,0 +1,7 @@
+package com.example.trpp.NavigationActivity.ui.queue;
+
+import androidx.lifecycle.ViewModel;
+
+public class QueueViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
