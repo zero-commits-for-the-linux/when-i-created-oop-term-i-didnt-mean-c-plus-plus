19899e69bae87f6a3cb46c3436ccf92fb68e6b77
Dmitry <lisyutin.dima@mail.com>
2022-03-13 20:47:46 +0300

    Добавлен выбор изображения из галереи. Космитические изменения экранов входа и регистрации.

--- app/src/main/java/com/example/trpp/NavigationActivity/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/profile/ProfileFragment.java
@@ -1,12 +1,20 @@
 package com.example.trpp.NavigationActivity.ui.profile;
 
 
+import static android.app.Activity.RESULT_OK;
+import static androidx.core.content.ContextCompat.getDrawable;
 import static androidx.core.content.ContextCompat.getSystemService;
 
+import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +22,23 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.trpp.R;
 import com.example.trpp.databinding.FragmentProfileBinding;
 
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    CircleImageView profileImage;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -34,6 +50,7 @@ public class ProfileFragment extends Fragment {
         ImageButton edit_btn = binding.profileEdit;
         ImageButton save_btn = binding.profileSave;
         ImageButton image_btn = binding.imageEdit;
+        ImageView profileImage = binding.profileImage;
 
         edit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +64,11 @@ public class ProfileFragment extends Fragment {
                 image_btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        //Вызываем стандартную галерею для выбора изображения с помощью Intent.ACTION_PICK:
                         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                        //Тип получаемых объектов - image:
                         photoPickerIntent.setType("image/*");
-                        //Запускаем переход с ожиданием обратного результата в виде информации об изображении:
-//                        startActivity(photoPickerIntent, image_btn.getRootWindowInsets());
+                        startActivityForResult(photoPickerIntent, 1);
                     }
+
                 });
 
                 save_btn.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +84,17 @@ public class ProfileFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        profileImage = binding.profileImage;
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = imageReturnedIntent.getData();
+                profileImage.setImageURI(selectedImage);
+            }
+        }
     }
 
     @Override
