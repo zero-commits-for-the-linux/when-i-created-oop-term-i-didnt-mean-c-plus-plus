341a3124dba1adecce00d94f99f0a806ba0dda42
Dmitry <lisyutin.dima@mail.com>
2022-03-21 23:08:45 +0300

    Обработчик событий для кнопки add_new_vote

---
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/votes/CreateVotes.java
@@ -0,0 +1,17 @@
+package com.example.trpp.NavigationActivity.ui.votes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trpp.R;
+
+public class CreateVotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_votes);
+
+    }
+}
--- app/src/main/java/com/example/trpp/NavigationActivity/ui/votes/VotesFragment.java
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/votes/VotesFragment.java
@@ -1,9 +1,12 @@
 package com.example.trpp.NavigationActivity.ui.votes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.example.trpp.databinding.FragmentVotesBinding;
@@ -16,6 +19,15 @@ public class VotesFragment extends Fragment {
 
         binding = FragmentVotesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        ImageButton btn_add = binding.addNewVote;
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), CreateVotes.class));
+            }
+        });
 
         return root;
     }
