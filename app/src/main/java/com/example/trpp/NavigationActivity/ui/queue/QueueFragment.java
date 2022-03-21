43383d2fa17dd3f25ff3f5ce82f0b59c43b9532b
Dmitry <lisyutin.dima@mail.com>
2022-03-21 18:46:44 +0300

    Мелкие косметические и логические изменения. Добавлен фрагмент опросов.

--- app/src/main/java/com/example/trpp/NavigationActivity/ui/queue/QueueFragment.java
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/queue/QueueFragment.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -16,18 +15,16 @@ import com.example.trpp.databinding.FragmentQueueBinding;
 
 public class QueueFragment extends Fragment {
 
-
     private FragmentQueueBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentQueueBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         Button to_queue = binding.toQueue;
         Button out_queue = binding.outQueue;
-        EditText input = binding.testFIO;
 
         to_queue.setOnClickListener(new View.OnClickListener() {
             @Override
