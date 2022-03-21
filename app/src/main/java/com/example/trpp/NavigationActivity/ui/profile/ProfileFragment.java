77baf6c3fed4c0a9e14c3bb56b344a68b8948913
Дмитрий Лисютин <54859565+selectionforcollection@users.noreply.github.com>
2022-03-21 23:49:52 +0300

    Косметика(Проверка имени)

--- app/src/main/java/com/example/trpp/NavigationActivity/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/profile/ProfileFragment.java
@@ -1,6 +1,5 @@
 package com.example.trpp.NavigationActivity.ui.profile;
 
-
 import static android.app.Activity.RESULT_OK;
 import android.content.Intent;
 import android.net.Uri;
@@ -8,7 +7,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import androidx.annotation.NonNull;
