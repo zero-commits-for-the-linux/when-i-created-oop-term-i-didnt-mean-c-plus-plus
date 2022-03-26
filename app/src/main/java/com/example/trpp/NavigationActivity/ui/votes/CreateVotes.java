578df2473e8a03bf3ebd81a9cab2dca2d24518dd
SelectionForCollection <lisyutin.dima@mail.ru>
2022-03-26 15:05:31 +0300

    адаптер для RecyclerView

--- app/src/main/java/com/example/trpp/NavigationActivity/ui/votes/CreateVotes.java
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/votes/CreateVotes.java
@@ -1,6 +1,10 @@
 package com.example.trpp.NavigationActivity.ui.votes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,27 +15,43 @@ import android.widget.ListView;
 
 import com.example.trpp.R;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CreateVotes extends AppCompatActivity {
-
+    static int COUNTER = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_votes);
 
         Button btn_add_var = findViewById(R.id.add_new_var);
-        ListView listView = findViewById(R.id.list_view);
+        RecyclerView mRecyclerView = findViewById(R.id.recycle);
+        String[] mDataSet = new String[3];
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
-        listView.setAdapter(adapter);
+        CustomAdapter adapter = new CustomAdapter(mDataSet);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(adapter);
 
 
         btn_add_var.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                adapter.add(findViewById(R.id.title_create_vote).toString());
-                adapter.notifyDataSetChanged();
+                COUNTER++;
+                if(COUNTER == 10){
+                    btn_add_var.setEnabled(false);
+                }else {
+                    String[] buffer = new String[COUNTER];
+                    String[] mDataSetNew = new String[]{""};
+//                    Arrays.copyOf(mDataSet, mDataSet.length + 1);
+                    for(int i = 0; i <mDataSet.length; i++) {
+                        buffer[i] = mDataSet[i];
+                    }
+                    buffer[COUNTER-1] = mDataSet[0];
+                    CustomAdapter adapter = new CustomAdapter(buffer);
+                    mRecyclerView.setAdapter(adapter);
+                }
             }
         });
     }
---
+++ app/src/main/java/com/example/trpp/NavigationActivity/ui/votes/CustomAdapter.java
@@ -0,0 +1,87 @@
+package com.example.trpp.NavigationActivity.ui.votes;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trpp.R;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+    private String[] mDataset;
+
+    public CustomAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public CustomAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        // pass MyCustomEditTextListener to viewholder in onCreateViewHolder
+        // so that we don't have to do this expensive allocation in onBindViewHolder
+        ViewHolder vh = new ViewHolder(v, new MyCustomEditTextListener());
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        // update MyCustomEditTextListener every time we bind a new item
+        // so that it knows what item in mDataset to update
+        holder.myCustomEditTextListener.updatePosition(holder.getAdapterPosition());
+        holder.mEditText.setText(mDataset[holder.getAdapterPosition()]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public EditText mEditText;
+        public MyCustomEditTextListener myCustomEditTextListener;
+
+        public ViewHolder(View v, MyCustomEditTextListener myCustomEditTextListener) {
+            super(v);
+
+            this.mEditText = (EditText) v.findViewById(R.id.list_item_editText);
+            this.myCustomEditTextListener = myCustomEditTextListener;
+            this.mEditText.addTextChangedListener(myCustomEditTextListener);
+        }
+    }
+
+    // we make TextWatcher to be aware of the position it currently works with
+    // this way, once a new item is attached in onBindViewHolder, it will
+    // update current position MyCustomEditTextListener, reference to which is kept by ViewHolder
+    private class MyCustomEditTextListener implements TextWatcher {
+        private int position;
+
+        public void updatePosition(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            // no op
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            mDataset[position] = charSequence.toString();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    }
+}
